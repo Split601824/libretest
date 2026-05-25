d208bf8d6bd8be3640588a10d03c0c3cd6faae65
Split601824 <split60pc@gmail.com>
2026-05-25 17:10:58 -0400

    v0.2.5 -- Fixed Studio vs Player bug

--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,12 @@ import AppStudio from './AppStudio';
 import './index.css';
 
 // Change this to 'studio' to test Studio
-const mode: 'player' | 'studio' = 'studio';
+const mode: 'player' | 'studio' = 'player';
 
-const App = mode === 'player' ? AppPlayer : AppStudio;
+const App = mode === 'studio' ? AppPlayer : AppStudio;
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <AppStudio />
+    <App />
   </React.StrictMode>
 );
